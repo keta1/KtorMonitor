39c16721620cb40a216a2463c4efe759244c1fd0
Cosmin Mihu <cosmin.mihu@gmail.com>
2025-01-26 22:19:32 +0200

    Refactor Summary Screen to improve error and loading states
    - Replace custom image with native icons
    - Add a specific loading state
    - Add a specific error state

--- composeApp/src/commonMain/kotlin/ro/cosminmihu/ktor/monitor/library/ui/detail/SummaryScreen.kt
+++ composeApp/src/commonMain/kotlin/ro/cosminmihu/ktor/monitor/library/ui/detail/SummaryScreen.kt
@@ -1,25 +1,20 @@
 package ro.cosminmihu.ktor.monitor.library.ui.detail
 
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.HourglassTop
+import androidx.compose.material.icons.filled.Error
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.key.Key.Companion.R
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.stringResource
@@ -34,25 +29,9 @@ fun SummaryScreen(summary: DetailUiState.Summary, modifier: Modifier = Modifier)
         KeyValue(key = "Method", value = summary.method)
         KeyValue(key = "Protocol", value = summary.protocol)
         when {
-            summary.isLoading ->
-                KeyValue(
-                    key = "Status",
-                    imageVector = Icons.Filled.HourglassTop,
-                    imageTint = MaterialTheme.colorScheme.tertiary,
-                    contentDescription = stringResource(Res.string.error),
-                    imageRotation = true,
-                )
-
-            summary.isError ->
-                KeyValue(
-                    key = "Status",
-                    imageVector = Icons.Filled.Warning,
-                    contentDescription = stringResource(Res.string.error),
-                    imageTint = MaterialTheme.colorScheme.error
-                )
-
-            else ->
-                KeyValue(key = "Response Code", value = summary.responseCode)
+            summary.isLoading -> KeyLoading(key = "Status")
+            summary.isError -> KeyError(key = "Status")
+            else -> KeyValue(key = "Response Code", value = summary.responseCode)
         }
         Spacer(modifier = Modifier.padding(8.dp))
         KeyValue(key = "Request time", value = summary.requestTime)
@@ -84,42 +63,41 @@ private fun KeyValue(key: String, value: String) {
     }
 }
 
-
 @Composable
-private fun KeyValue(
-    key: String,
-    imageVector: ImageVector,
-    imageTint: Color,
-    contentDescription: String?,
-    imageRotation: Boolean = false,
-) {
-    when {
-        imageRotation -> {
-            val infiniteTransition = rememberInfiniteTransition(label = "Infinite rotation")
-            val rotationAnimation by infiniteTransition.animateFloat(
-                initialValue = 0f,
-                targetValue = 360f,
-                animationSpec = infiniteRepeatable(animation = tween(durationMillis = 1000)),
-                label = "Rotation"
-            )
-            rotationAnimation
-        }
-
-        else -> 0f
-    }
-
+private fun KeyLoading(key: String) {
     Row(
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
             text = key,
-
             fontWeight = FontWeight.Bold,
             modifier = Modifier.weight(0.3f),
             style = MaterialTheme.typography.bodyMedium,
         )
         Box(modifier = Modifier.weight(0.7f)) {
             Loading.Small()
+        }
+    }
+}
+
+
+@Composable
+private fun KeyError(key: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = key,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.weight(0.3f),
+            style = MaterialTheme.typography.bodyMedium,
+        )
+        Box(modifier = Modifier.weight(0.7f)) {
+            Icon(
+                imageVector = Icons.Filled.Warning,
+                contentDescription = stringResource(Res.string.error),
+                tint = MaterialTheme.colorScheme.error,
+            )
         }
     }
 }
