5525b3244630733b0ec45209d4b655a89f24ca2e
Cosmin Mihu <cosmin.mihu@gmail.com>
2025-01-31 23:35:02 +0200

    Update android sample app to use edge to edge and add a header.

--- samples/composeApp/src/androidMain/kotlin/ro/cosminmihu/ktor/monitor/sample/SampleActivity.kt
+++ samples/composeApp/src/androidMain/kotlin/ro/cosminmihu/ktor/monitor/sample/SampleActivity.kt
@@ -3,8 +3,17 @@ package ro.cosminmihu.ktor.monitor.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ro.cosminmihu.ktor.monitor.KtorMonitor
 
 /**
@@ -13,9 +22,25 @@ import ro.cosminmihu.ktor.monitor.KtorMonitor
 class SampleActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         setContent {
-            App()
+            Scaffold {
+                Column(
+                    modifier = Modifier.padding(it),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Sample how to use Ktor Monitor for Android",
+                        modifier = Modifier.padding(vertical = 16.dp)
+                    )
+                    Box(
+                        contentAlignment = Alignment.Center
+                    ) {
+                        App()
+                    }
+                }
+            }
         }
     }
 }
