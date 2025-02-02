3cd5102e236804e9e26538f70608deca3e5f7781
Cosmin Mihu <cosmin.mihu@gmail.com>
2025-02-02 16:21:02 +0200

    Add `distinctUntilChanged` to `GetCallUseCase` flow to reduce duplicate emissions.

--- ktor-monitor/library/src/commonMain/kotlin/ro/cosminmihu/ktor/monitor/domain/GetCallUseCase.kt
+++ ktor-monitor/library/src/commonMain/kotlin/ro/cosminmihu/ktor/monitor/domain/GetCallUseCase.kt
@@ -1,5 +1,6 @@
 package ro.cosminmihu.ktor.monitor.domain
 
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import ro.cosminmihu.ktor.monitor.db.LibraryDao
 
@@ -9,4 +10,20 @@ internal class GetCallUseCase(
 
     operator fun invoke(id: String) = dao.getCall(id)
         .map { it.executeAsOneOrNull() }
+        .distinctUntilChanged { old, new ->
+            old?.id == new?.id &&
+                old?.method == new?.method &&
+                old?.url == new?.url &&
+                old?.protocol == new?.protocol &&
+                old?.requestTimestamp == new?.requestTimestamp &&
+                old?.requestHeaders == new?.requestHeaders &&
+                old?.requestContentType == new?.requestContentType &&
+                old?.requestContentLength == new?.requestContentLength &&
+                old?.responseCode == new?.responseCode &&
+                old?.responseTimestamp == new?.responseTimestamp &&
+                old?.responseContentType == new?.responseContentType &&
+                old?.responseHeaders == new?.responseHeaders &&
+                old?.responseContentLength == new?.responseContentLength &&
+                old?.error == new?.error
+        }
 }
