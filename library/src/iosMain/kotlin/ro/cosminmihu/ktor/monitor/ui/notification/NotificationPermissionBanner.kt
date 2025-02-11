7e120a3757ed6e792845bd8b30a9f8e473ff34cf
Cosmin Mihu <cosmin.mihu@gmail.com>
2025-02-11 20:00:58 +0200

    Implement Notification Permission Banner for iosz

--- library/src/iosMain/kotlin/ro/cosminmihu/ktor/monitor/ui/notification/NotificationPermissionBanner.kt
+++ library/src/iosMain/kotlin/ro/cosminmihu/ktor/monitor/ui/notification/NotificationPermissionBanner.kt
@@ -4,29 +4,25 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.stringResource
 import platform.UserNotifications.UNUserNotificationCenter
 import ro.cosminmihu.ktor.monitor.ui.Dimens
 import ro.cosminmihu.ktor.monitor.ui.resources.Res
 import ro.cosminmihu.ktor.monitor.ui.resources.ktor_permission_message
-import ro.cosminmihu.ktor.monitor.ui.resources.ktor_permission_grant
 
 @Composable
 internal actual fun NotificationPermissionBanner() {
     val coroutineScope = rememberCoroutineScope()
     var isGranted by remember { mutableStateOf(true) }
+
+    LaunchedEffect(Unit) {
+        isGranted = UNUserNotificationCenter.currentNotificationCenter().isNotificationPermissionGranted()
+    }
 
     AnimatedVisibility(visible = !isGranted) {
         Row(
@@ -39,15 +35,6 @@ internal actual fun NotificationPermissionBanner() {
                 modifier = Modifier.weight(1f),
                 text = stringResource(Res.string.ktor_permission_message),
             )
-            Button(
-                onClick = {
-                    coroutineScope.launch {
-                        isGranted = UNUserNotificationCenter.currentNotificationCenter().isNotificationPermissionGranted()
-                    }
-                }
-            ) {
-                Text(text = stringResource(Res.string.ktor_permission_grant))
-            }
         }
     }
 }
